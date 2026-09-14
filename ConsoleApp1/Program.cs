2f8fea6608fd0685bb743f3ec56cfdd2207af52c
MariiaLysiana <MariiaLysiana@TE-GDA-D-D21E24>
2026-09-14 14:59:01 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string[] nazwy = { "Procesor", "Pamięć RAM", "Dysk SSD", "Zasilacz", "Karta graficzna" };
+double[] ceny = { 899.00, 249.50, 379.00, 189.99, 599.00 };
+
+double suma = 0;
+int licznik = 0;
+
+for (int i = 0; i < nazwy.Length; i++)
+{
+    // Do sumy trafiają tylko produkty droższe niż 200 zł
+    if (ceny[i] > 200)
+    {
+        suma = suma + ceny[i];
+        licznik++;
+    }
+}
+
+// Uwaga: przy pustym liczniku byłoby dzielenie przez zero
+double srednia = suma / licznik;
+Console.WriteLine($"Średnia cena: {srednia:F2} zł z {licznik} produktów");
