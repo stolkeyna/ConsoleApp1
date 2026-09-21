0eba647336591cbd9c9caaafade86595873c3a9a
MariiaLysiana <MariiaLysiana@TE-GDA-D-D21E24>
2026-09-21 11:36:52 +0200

    Aaa

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//zakres nazw w którym pracujemy, w którym nazwy są nie powtarzalne
+namespace ConsoleApp1
+{
+    //internal jest opcjonalne
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public string Categoty;
+        public int Ammount;
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,21 +1,78 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿using ConsoleApp1;
+using System.Diagnostics.CodeAnalysis;
 
-string[] nazwy = { "Procesor", "Pamięć RAM", "Dysk SSD", "Zasilacz", "Karta graficzna" };
-double[] ceny = { 899.00, 249.50, 379.00, 189.99, 599.00 };
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-double suma = 0;
-int licznik = 0;
+//string[] nazwy = { "Procesor", "Pamięć RAM", "Dysk SSD", "Zasilacz", "Karta graficzna" };
+//double[] ceny = { 899.00, 249.50, 379.00, 189.99, 599.00 };
 
-for (int i = 0; i < nazwy.Length; i++)
+//utwórz nowy obiekt processor według definicji klasy Product
+Product processor = new Product();
+//nadaj mu wartości pól
+processor.Name = "AMD Ryzen";
+processor.Price = 890.00;
+processor.Categoty = "Podzespoły";
+processor.Ammount = 10;
+
+//można też tak
+Product ram = new Product
 {
-    // Do sumy trafiają tylko produkty droższe niż 200 zł
-    if (ceny[i] > 200)
-    {
-        suma = suma + ceny[i];
-        licznik++;
-    }
-}
+    Name = "Pamięć RAM",
+    Price = 250.00,
+    Categoty = "Podzespoły",
+    Ammount = 15
+};
 
-// Uwaga: przy pustym liczniku byłoby dzielenie przez zero
-double srednia = suma / licznik;
-Console.WriteLine($"Średnia cena: {srednia:F2} zł z {licznik} produktów");
+Product ssd = new Product
+{
+    Name = "Dysk ssd",
+    Price = 360.99,
+    Categoty = "Podzespoły",
+    Ammount = 27
+};
+Product zasilacz = new Product
+{
+    Name = "Zasilacz",
+    Price = 190.00,
+    Categoty = "Podzespoły",
+    Ammount = 13
+};
+
+//tworzymy tablice produktów
+Product[] products = {processor, ram, ssd, zasilacz};
+
+double sum = 0;
+int count = 0;
+double LowestPrice = 0;
+foreach (Product product in products)
+{
+    Console.WriteLine($"Nazwa: {product.Name, -25}| Cena: {product.Price, 10:F2}| Kategoria: {product.Categoty}| Ilość: {product.Ammount, 5}");
+    sum += product.Price;
+    count++;
+    for(int i = 0; i<products.Length; i++)
+    {
+        LowestPrice = product.Price;
+        if (product.Price < LowestPrice)
+        {
+            LowestPrice = product.Price;
+        }
+    }
+
+}
+double average = sum / count;
+//Console.WriteLine($"Najtańszy: {}| cena średnia: {}| najdroższy: {}");
+//for (int i = 0; i < nazwy.Length; i++)
+//{
+//    // Do sumy trafiają tylko produkty droższe niż 200 zł
+//    if (ceny[i] > 200)
+//    {
+//        //1148.5
+//        suma = suma + ceny[i];
+//        licznik++;
+//    }
+//}
+
+//// Uwaga: przy pustym liczniku byłoby dzielenie przez zero
+//double srednia = suma / licznik;
+//Console.WriteLine($"Ilość produktów w bazie: {nazwy.Length}.");
+//Console.WriteLine($"Średnia cena: {srednia:F2} zł z {licznik} produktów droższych od 200zł.");
